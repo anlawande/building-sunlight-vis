66c928d0fb199db5cbb6398c596b73bb649f52c2
Aniket Lawande <anlawande@gmail.com>
2022-05-23 07:58:59 -0700

    Working shadows and slider for light

--- public/main.js
+++ public/main.js
@@ -4,26 +4,29 @@ import { OrbitControls } from '/OrbitControls.js';
 
 let camera, controls, scene, renderer;
 
-let plane;
-let cube;
+let plane, cube, light;
 
 init();
 //render(); // remove when using next line for animation loop (requestAnimationFrame)
 animate();
 
-function init() {
+addSunControls();
+
+function init(array, offset) {
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xcccccc );
     // scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
 
     renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.PCFShadowMap;
     renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
 
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
-    camera.position.set( -10,-10, 100 );
+    camera.position.set(0, 0, 100);
 
     // controls
 
@@ -45,30 +48,39 @@ function init() {
     // world
 
     var geometry = new THREE.BoxGeometry(10, 10, 10);
-    var material = new THREE.MeshPhongMaterial({color: 0x00ff00});
+    var material = new THREE.MeshStandardMaterial({color: 0x00ff00});
     cube = new THREE.Mesh(geometry, material);
+    cube.position.y = 20;
+    cube.castShadow = true;
+    cube.receiveShadow = true;
     scene.add(cube);
     scene.background = new THREE.Color(0xffffff);
 
     plane = new THREE.Mesh(
         new THREE.PlaneGeometry(50, 50),
         new THREE.MeshStandardMaterial({
-            color: 0xffff00,
+            color: 0xff0000,
         }));
     // plane.castShadow = false;
     // plane.receiveShadow = true;
-    plane.rotation.x = -Math.PI / 2;
+    plane.rotation.x = -Math.PI / 2
+    plane.castShadow = false;
+    plane.receiveShadow = true;
     scene.add(plane);
 
     // lights
 
     const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(-1, 2, 4);
+    const intensity = 100;
+    light = new THREE.PointLight( color, intensity, 200 );
+    light.position.set(0, 80, 0);
+    light.castShadow = true;
     scene.add(light);
+    const helper = new THREE.PointLightHelper( light, 50, 0xFF0000 );
+    scene.add( helper );
 
-    //
+    const axesHelper = new THREE.AxesHelper( 50 );
+    scene.add( axesHelper );
 
     window.addEventListener( 'resize', onWindowResize );
 
@@ -77,7 +89,7 @@ function init() {
 function onWindowResize() {
 
     camera.aspect = window.innerWidth / window.innerHeight;
-    // camera.updateProjectionMatrix();
+    camera.updateProjectionMatrix();
 
     renderer.setSize( window.innerWidth, window.innerHeight );
 
@@ -87,7 +99,8 @@ function animate() {
 
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
+    // cube.rotation.x += 0.01;
+    // light.position.x += 0.1;
     controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
 
     render();
@@ -98,4 +111,14 @@ function render() {
 
     renderer.render( scene, camera );
 
+}
+
+function addSunControls() {
+    const dayTimeSlider = document.querySelector('#dayTimeSlider');
+    dayTimeSlider.addEventListener('input', onChangeDayTimeSlider);
+}
+
+function onChangeDayTimeSlider(event) {
+    const value = event.target.value;
+    light.position.x = value;
 }
